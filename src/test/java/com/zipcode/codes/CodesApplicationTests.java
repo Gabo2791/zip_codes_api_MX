5383c78cc7971c85c42e80a53764325f95b88589
Gabriel Morales <gbmres2704@gmail.com>
2023-12-20 19:27:27 -0600

    First approach of zipcodes API

---
+++ src/test/java/com/zipcode/codes/CodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
